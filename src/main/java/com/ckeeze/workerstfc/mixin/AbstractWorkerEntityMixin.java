b2b3b5e4d5e17eb892c113c08b855d138daada89
ckeeze <33778184+ckeeze@users.noreply.github.com>
2026-02-11 10:50:08 +0100

    Nerf NPC hunger rate
    Nerfed the rate of food consumption by workers

--- src/main/java/com/ckeeze/workerstfc/mixin/AbstractWorkerEntityMixin.java
+++ src/main/java/com/ckeeze/workerstfc/mixin/AbstractWorkerEntityMixin.java
@@ -2,12 +2,29 @@ package com.ckeeze.workerstfc.mixin;
 
 import com.talhanation.workers.entities.AbstractWorkerEntity;
 
+import net.minecraft.core.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.Optional;
 
 @SuppressWarnings("unused")
 @Mixin(AbstractWorkerEntity.class)
 public abstract class AbstractWorkerEntityMixin {
+
+    @Shadow
+    public BlockPos getBedPos() {
+        return null;
+    }
+    @Shadow
+    public AbstractWorkerEntity.Status getStatus() {
+        return null;
+    };
+    @Shadow
+    public int getHunger(){return 0;};
+    @Shadow
+    public void setHunger(int hunger){}
 
     /**
      * @author Ckeeze
@@ -16,5 +33,22 @@ public abstract class AbstractWorkerEntityMixin {
     @Overwrite(remap = false)
     protected int getFarmedItemsDepositAmount() {
         return 32;
+    }
+
+    @Overwrite(remap =false)
+    public void updateHunger() {
+        AbstractWorkerEntity.Status status = this.getStatus();
+        int hunger = this.getHunger();
+        if (status != AbstractWorkerEntity.Status.WORK && this.getBedPos() != null) {
+            hunger -= 1;
+        } else {
+            hunger -= 2;
+        }
+
+        if (hunger < 0) {
+            hunger = 0;
+        }
+
+        this.setHunger(hunger);
     }
 }
